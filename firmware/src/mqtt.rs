9f09c454db37c8d18f6ba6c0cdccb980ba971b28
David Košťál <kostal.david8@gmail.com>
2024-05-02 16:00:25 +0800

    connect to AWS IoT MQTT

--- firmware/src/mqtt.rs
+++ firmware/src/mqtt.rs
@@ -1,1 +1,53 @@
+use std::ffi::CStr;
 
+use crate::{AWS_CERT, AWS_PRIVKEY, CONFIG, AWS_ROOT1};
+
+use esp_idf_svc::mqtt::client::{EspMqttClient, MqttClientConfiguration, QoS};
+use esp_idf_svc::tls::X509;
+use log::info;
+
+pub struct Mqtt {
+    client: EspMqttClient<'static>,
+}
+
+impl Mqtt {
+    pub fn connect() -> anyhow::Result<Self> {
+        // Client configuration:
+        let broker_url = format!("mqtts://{}", CONFIG.aws_endpoint);
+        info!("Connecting to MQTT broker {broker_url}...");
+
+        let mut mqtt_config = MqttClientConfiguration::default();
+        let cert = CStr::from_bytes_with_nul(AWS_CERT)?;
+        let key = CStr::from_bytes_with_nul(AWS_PRIVKEY)?;
+        let root = CStr::from_bytes_with_nul(AWS_ROOT1)?;
+
+        mqtt_config.client_certificate =
+            Some(X509::pem(&cert));
+        mqtt_config.private_key =
+            Some(X509::pem(&key));
+        mqtt_config.server_certificate =
+            Some(X509::pem(&root));
+
+        mqtt_config.client_id = Some("basket");
+
+
+        let mut client =
+        EspMqttClient::new_cb(
+            &broker_url,
+            &mqtt_config,
+            move |message_event| match message_event {
+                _ => info!("Received from MQTT: {:?}", message_event.payload()),
+            },
+        )?;
+    
+        info!("MQTT connected!");
+        client.subscribe("test", QoS::AtMostOnce)?;
+        info!("MQTT subscribed!");
+
+        // 2. publish an empty hello message
+        let payload: &[u8] = &[];
+        client.publish("hello", QoS::AtMostOnce, true, payload)?;
+
+        Ok(Self { client })
+    }
+}
